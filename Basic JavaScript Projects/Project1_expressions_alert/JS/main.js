54a5dc374806a3ed0228b57139447e006daf50da
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-08-22 10:01:18 +0100

    Update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+window.alert ("jolibee") ;  //Displays a box with a message and an OK button.
+
+var blues = "blue arrrrmyyyy <br>";     //Defining a variable and giving it a string value.
+var blues = blues.fontcolor("blue")     //Using the font color method on blues variable.
+document.write (blues)                  //Writes out the variable.
+
+var green = "String ting <br>";         //Defining a variable and giving it a string value.
+var green = green.fontcolor("green")    //Using the font color method on green variable.
+document.write (green);                 //Writes out the variable.
+
+var pink = ('Dad shouted, "Kids, dinner is ready!" <br>')    //Defining a variable and giving it a string value.
+var pink = pink.fontcolor("pink")       //Using the font color method on pink variable.
+document.write (pink)                   //Writes out the variable.
+
+var red = ('\"Help me!\"'               //Defining a variable and giving it a string value.
++ 'my dude <br>')                       //Defining a variable and giving it a string value.
+var red = red.fontcolor("red")          //Using the font color method on red variable.
+document.write (red)                    //Writes out the variable.
+
+var orange = ('"Big" + "Lad"<br>')      //Defining a variable and giving it a string value.
+var orange = orange.fontcolor("orange") //Using the font color method on orange variable.
+document.write (orange)                 //Writes out the variable.
+
+var Family = "The Becketts"             //Defining a variable and giving it a string value.
+var Dad = "James"                       //Defining a variable and giving it a string value.
+var Mum = "Rumey"                       //Defining a variable and giving it a string value.
+var Daughter = "Trininty"               //Defining a variable and giving it a string value.
+var Son = "Bentley"                     //Defining a variable and giving it a string value.
+
+document.write(Mum)                     //Writes out the variable.
+
+document.write(5 + 9);                   //Writes out the expression.
